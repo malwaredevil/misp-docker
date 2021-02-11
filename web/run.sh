d102177259bd1c6a388d25f00bc05044dccc1478
Jonathan Batchelor <jonathan.batchelor@sil.lab>
2021-02-11 14:47:54 -0600

    202102111447

--- web/run.sh
+++ web/run.sh
@@ -287,11 +287,11 @@ fi
 /var/www/MISP/app/Console/cake live 1
 chown www-data:www-data /var/www/MISP/app/Config/config.php*
 
-# Start supervisord
-echo "Starting supervisord"
-cd /
-/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf &
-exit 0
+
+# # Start supervisord
+# echo "Starting supervisord"
+# cd /
+# exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
           
 # TO CONTINUE RUNNING AFTER THIS SCRIPT COMPLETES UNCOMMENT THE NEXT LINE
 
